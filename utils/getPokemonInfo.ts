3719117c3b17e5f2947dc51b48e22c25537012ad
Kevin Garcia <gkevinyamil@gmail.com>
2022-10-29 16:49:09 -0600

    Static site generation

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -1,15 +1,18 @@
-import { pokeApi } from '../api';
-import { Pokemon } from '../interfaces';
+import { pokeApi } from "../api";
+import { Pokemon } from "../interfaces";
 
-export const getPokemonInfo = async(nameOrId:string) => { 
-    
-	const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+export const getPokemonInfo = async (nameOrId: string) => {
+	try {
+		const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
 
-	return {
-		id: data.id,
-		moves: data.moves,
-		name: data.name,
-		sprites: data.sprites,
-		stats: data.stats
+		return {
+			id: data.id,
+			moves: data.moves,
+			name: data.name,
+			sprites: data.sprites,
+			stats: data.stats,
+		};
+	} catch (error) {
+		return null;
 	}
- }
+};
